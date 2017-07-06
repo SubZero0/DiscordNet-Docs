6c925ef094c7f148c491b97852ec9c1ff895ec2a
Paulo Anjos <pnmanjos@hotmail.com>
2017-07-06 00:18:19 -0300

    Forgot to push this one

--- src/DiscordNet/Utils.cs
+++ src/DiscordNet/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace DiscordNet
 {
@@ -10,5 +11,46 @@ namespace DiscordNet
         {
             return source.Select(t => new {Index = Guid.NewGuid(), Value = t}).OrderBy(p => p.Index).Select(p => p.Value);
         }
+
+        public static string BuildType(Type type)
+        {
+            string typeName = type.Name, typeGeneric = "";
+            int idx;
+            if ((idx = typeName.IndexOf('`')) != -1)
+            {
+                typeName = typeName.Substring(0, idx);
+                var generics = type.GetGenericArguments();
+                if (generics.Any())
+                    typeGeneric = $"<{String.Join(", ", generics.Select(x => BuildType(x)))}>";
+            }
+            return GetTypeName(type, typeName, typeGeneric);
+        }
+
+        private static string GetTypeName(Type type, string name, string generic)
+        {
+            if (type.IsInstanceOfType(typeof(Nullable<>)))
+                return $"{generic}?";
+            return Aliases.ContainsKey(type) ? Aliases[type] : $"{name}{generic}";
+        }
+
+        private static readonly Dictionary<Type, string> Aliases = new Dictionary<Type, string>()
+        {
+            { typeof(byte), "byte" },
+            { typeof(sbyte), "sbyte" },
+            { typeof(short), "short" },
+            { typeof(ushort), "ushort" },
+            { typeof(int), "int" },
+            { typeof(uint), "uint" },
+            { typeof(long), "long" },
+            { typeof(ulong), "ulong" },
+            { typeof(float), "float" },
+            { typeof(double), "double" },
+            { typeof(decimal), "decimal" },
+            { typeof(object), "object" },
+            { typeof(bool), "bool" },
+            { typeof(char), "char" },
+            { typeof(string), "string" },
+            { typeof(void), "void" }
+        };
     }
 }
